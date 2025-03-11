570255b86f8fc10dad97d112e25f031833206969
patrikvalentiny <patrikvalentiny@gmail.com>
2025-03-11 12:55:52 +0100

    Add Docker Compose files for monitoring, messaging, and database services; remove unused CleanerRepository

--- src/CleanerService/Application/Services/CleanerService.cs
+++ src/CleanerService/Application/Services/CleanerService.cs
@@ -6,20 +6,26 @@ using SharedModels;
 
 namespace CleanerService.Application.Services;
 
-public class CleanerService(CleanedMessagePublisher messagePublisher) {
+public class CleanerService(CleanedMessagePublisher messagePublisher)
+{
 
-    public async Task<IEnumerable<CleanedFileDTO>> CleanFilesAsync(string path = "../../data") {
-        
-        using var activity = MonitoringService.ActivitySource.StartActivity("CleanerService.CleanFilesAsync");
-        string[] allFiles = Directory.GetFiles(path, "", SearchOption.AllDirectories);
-        Log.Logger.Information("Cleaning {FileCount} files", allFiles.Length);
-        var paths = allFiles.Select(Path.GetFullPath).Take(10);
+    public async Task<IEnumerable<CleanedFileDTO>> CleanFilesAsync(string path = "../../data")
+    {
+         var paths = new List<string>();
+        using (MonitoringService.ActivitySource.StartActivity("LoadPaths"))
+        {
+            string[] allFiles = Directory.GetFiles(path, "", SearchOption.AllDirectories);
+            Log.Logger.Information("Cleaning {FileCount} files", allFiles.Length);
+            paths = allFiles.Select(Path.GetFullPath).Take(10).ToList();
+        }
+        using var activity = MonitoringService.ActivitySource.StartActivity("CleanFilesAsync");
         var tasks = paths.Select(CleanFileAsync);
         return await Task.WhenAll(tasks);
     }
 
-    private async Task<CleanedFileDTO> CleanFileAsync(string path) {
-        using var activity = MonitoringService.ActivitySource.StartActivity("CleanerService.CleanFileAsync");
+    private async Task<CleanedFileDTO> CleanFileAsync(string path)
+    {
+        using var activity = MonitoringService.ActivitySource.StartActivity("CleanFileAsync");
         var message = await MimeMessage.LoadAsync(path);
         var cleanedContent = message.TextBody;
         var filename = Path.GetFileName(path);
@@ -28,12 +34,16 @@ public class CleanerService(CleanedMessagePublisher messagePublisher) {
         var parentFolder = string.Join("_", pathParts.Skip(dataIndex + 1));
         var cleanedFilename = $"{parentFolder}_{filename[..^1]}.txt";
         Log.Logger.Information("Cleaning file {Filename}", cleanedFilename);
-        return new CleanedFileDTO { Filename = cleanedFilename, Content = cleanedContent };
+        var cleanedFIleDTO = new CleanedFileDTO { Filename = cleanedFilename, Content = cleanedContent };
+        await messagePublisher!.PublishCleanedMessage(cleanedFIleDTO);
+        return cleanedFIleDTO;
     }
 
-    public async Task PublishCleanedFilesAsync(IEnumerable<CleanedFileDTO> cleanedFiles) {
-        using var activity = MonitoringService.ActivitySource.StartActivity("CleanerService.PublishCleanedFilesAsync");
-        await Parallel.ForEachAsync(cleanedFiles, async (cleanedFile, cancellationToken) => {
+    public async Task PublishCleanedFilesAsync(IEnumerable<CleanedFileDTO> cleanedFiles)
+    {
+        using var activity = MonitoringService.ActivitySource.StartActivity("PublishCleanedFilesAsync");
+        await Parallel.ForEachAsync(cleanedFiles, async (cleanedFile, cancellationToken) =>
+        {
             Log.Logger.Information("Publishing cleaned file {Filename}", cleanedFile.Filename);
             await messagePublisher!.PublishCleanedMessage(cleanedFile);
         });
